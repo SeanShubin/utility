f73eb2e57afcfe14a207dfbc8b89bfcf69ec748c
SeanShubin <sshubin@cj.com>
2015-02-24 14:09:12 -0800

    Reflection represents case classes as Seq of Tuple2 rather than Map, so argument list order is preserved

--- reflection/src/main/scala/com/seanshubin/utility/reflection/ReflectionImpl.scala
+++ reflection/src/main/scala/com/seanshubin/utility/reflection/ReflectionImpl.scala
@@ -25,10 +25,10 @@ class ReflectionImpl(simpleTypeConversions: Map[universe.Type, SimpleTypeConvers
     result
   }
 
-  private def pieceTogetherCaseClass(valueMap: Map[String, Any], tpe: universe.Type): Any = {
+  private def pieceTogetherCaseClass(values: Any, tpe: universe.Type): Any = {
     val constructor: universe.MethodSymbol = tpe.decl(universe.termNames.CONSTRUCTOR).asMethod
     val constructorParameters: Seq[universe.TermSymbol] = constructor.typeSignature.paramLists.head.map(_.asTerm)
-    val parameterList: Seq[Any] = createParameterList(constructorParameters, valueMap)
+    val parameterList: Seq[Any] = createParameterList(constructorParameters, values)
     val typeClass: universe.ClassSymbol = tpe.typeSymbol.asClass
     val classMirror: universe.ClassMirror = mirror.reflectClass(typeClass)
     val constructorMethod: universe.MethodMirror = classMirror.reflectConstructor(constructor)
@@ -65,7 +65,11 @@ class ReflectionImpl(simpleTypeConversions: Map[universe.Type, SimpleTypeConvers
     staticMap
   }
 
-  private def createParameterList(constructorParameters: Seq[universe.TermSymbol], valueMap: Map[String, Any]): Seq[Any] = {
+  private def createParameterList(constructorParameters: Seq[universe.TermSymbol], values: Any): Seq[Any] = {
+    val valueMap = values match {
+      case x: Map[_, _] => values.asInstanceOf[Map[String, Any]]
+      case x: Seq[_] => values.asInstanceOf[Seq[(String, Any)]].toMap
+    }
     def lookupValue(term: universe.TermSymbol): Any = {
       val parameterName = symbolName(term)
       val parameterType: universe.Type = term.info
@@ -87,7 +91,7 @@ class ReflectionImpl(simpleTypeConversions: Map[universe.Type, SimpleTypeConvers
     result
   }
 
-  private def pullApartCaseClass(value: Any, tpe: universe.Type): Map[String, Any] = {
+  private def pullApartCaseClass(value: Any, tpe: universe.Type): Seq[(String, Any)] = {
     val fields: Iterable[universe.TermSymbol] = tpe.decls.map(_.asTerm).filter(_.isGetter)
     val instanceMirror: universe.InstanceMirror = mirror.reflect(value)
     def createEntry(field: universe.TermSymbol): (String, Any) = {
@@ -100,8 +104,7 @@ class ReflectionImpl(simpleTypeConversions: Map[universe.Type, SimpleTypeConvers
       entry
     }
     val entries: Iterable[(String, Any)] = fields.map(createEntry)
-    val map: Map[String, Any] = entries.toMap
-    map
+    entries.toSeq
   }
 
   private def pullApartOption(value: Option[Any], tpe: universe.Type): Any = {
@@ -165,7 +168,7 @@ class ReflectionImpl(simpleTypeConversions: Map[universe.Type, SimpleTypeConvers
       pullApartCaseClass(staticValue, tpe)
 
     override def pieceTogetherAny(dynamicValue: Any, tpe: universe.Type): Any =
-      pieceTogetherCaseClass(dynamicValue.asInstanceOf[Map[String, Any]], tpe)
+      pieceTogetherCaseClass(dynamicValue, tpe)
   }
 
   private object ComplexSeq extends Complex {
