ab16ed531d2a7e297e0c2df427e071fc603102c5
SeanShubin <seanshubin@gmail.com>
2015-03-01 10:36:18 -0800

    Removed exception tracker

--- exception/src/main/scala/com/seanshubin/utility/exception/MutableExceptionTracker.scala
+++
@@ -1,7 +0,0 @@
-package com.seanshubin.utility.exception
-
-trait MutableExceptionTracker {
-  def addException(exception: Throwable): Unit
-
-  def staleValue: ExceptionTracker
-}
--- exception/src/main/scala/com/seanshubin/utility/exception/QuantifiedException.scala
+++
@@ -1,3 +0,0 @@
-package com.seanshubin.utility.exception
-
-case class QuantifiedException(exception: Throwable, quantity: Int)
