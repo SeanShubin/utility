ca968f094b3c0a661877bfddfaddaad72882b63d
SeanShubin <sshubin@cj.com>
2015-08-12 09:42:06 -0700

    Clarified why the contract patterns should not be concerned with abstractions.

--- system/src/main/scala/com.seanshubin.utility.system/ClockContractIntegration.scala
+++ system/src/main/scala/com.seanshubin.utility.system/ClockContractIntegration.scala
@@ -3,12 +3,13 @@ package com.seanshubin.utility.system
 import java.time.{Clock, Instant, ZoneId}
 
 /*
-This is a "contract integration" class
+This is part of a "contract" and "contract integration" pattern
 The sole purpose of these types of classes is to integrate the environment outside of our control with a contract (trait/interface/protocol)
 What belongs here are methods that are a one-to-one pass through to the system implementation
 What does NOT belong here is logic, wrappers, helpers, utilities, method chaining, etc.
 If you need any of that, put them in a separate cass that delegates to this one
 The contract can be used when implementing fakes, mocks, and stubs
+Proper abstractions can be implemented with full test coverage by delegating to these low level wrappers
 */
 class ClockContractIntegration(clock: Clock) extends ClockContract {
   override def getZone: ZoneId = clock.getZone
--- system/src/main/scala/com.seanshubin.utility.system/RandomContractIntegration.scala
+++ system/src/main/scala/com.seanshubin.utility.system/RandomContractIntegration.scala
@@ -4,12 +4,13 @@ import java.util.Random
 import java.util.stream.{DoubleStream, IntStream, LongStream}
 
 /*
-This is a "contract integration" class
+This is part of a "contract" and "contract integration" pattern
 The sole purpose of these types of classes is to integrate the environment outside of our control with a contract (trait/interface/protocol)
 What belongs here are methods that are a one-to-one pass through to the system implementation
 What does NOT belong here is logic, wrappers, helpers, utilities, method chaining, etc.
 If you need any of that, put them in a separate cass that delegates to this one
 The contract can be used when implementing fakes, mocks, and stubs
+Proper abstractions can be implemented with full test coverage by delegating to these low level wrappers
 */
 class RandomContractIntegration(random: Random) extends RandomContract {
   override def nextBytes(bytes: Array[Byte]): Unit = random.nextBytes(bytes)
