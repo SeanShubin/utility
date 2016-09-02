c3041506a587677603ea34d7b093159a6fb7a58c
Sean Shubin <sshubin@cj.com>
2016-09-02 12:15:44 -0700

    hex to bytes

--- string/src/main/scala/com/seanshubin/utility/string/StringUtil.scala
+++ string/src/main/scala/com/seanshubin/utility/string/StringUtil.scala
@@ -29,12 +29,28 @@ object StringUtil {
       replaceAll( """\\\\""", "\\")
   }
 
-  def hex(target: Seq[Byte]): String = {
-    target.map(hex).mkString
+  def bytesToHex(target: Seq[Byte]): String = {
+    target.map(byteToHex).mkString
   }
 
-  def hex(target: Byte): String = {
+  def byteToHex(target: Byte): String = {
     val digits = "0123456789ABCDEF"
     "" + digits(target >> 4 & 15) + digits(target & 15)
+  }
+
+  def hexToBytes(target:String):Seq[Byte] = {
+    target.grouped(2).map(hexToByte).toSeq
+  }
+
+  def hexToByte(rawTarget:String):Byte = {
+    if(rawTarget.length != 2) throw new RuntimeException("hexToByte expectes exactly 2 characters")
+    val target = rawTarget.toUpperCase
+    val numericTuples = 0 to 9 map (x => x.toString.head -> x)
+    val alphaTuples = 'A' to 'F' map (x => x -> (x - 'A' + 10))
+    val digitsMap = (numericTuples ++ alphaTuples).toMap
+    val firstDigit = digitsMap(target(0))
+    val secondDigit = digitsMap(target(1))
+    val result = firstDigit * 16 + secondDigit
+    result.toByte
   }
 }
