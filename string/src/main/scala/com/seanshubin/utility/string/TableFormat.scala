f82044e4e52e669adcbcd8ce863081fa3028413e
Sean Shubin <sshubin@cj.com>
2018-06-01 10:40:52 -0700

    configurable tableformat

---
+++ string/src/main/scala/com/seanshubin/utility/string/TableFormat.scala
@@ -0,0 +1,148 @@
+package com.seanshubin.utility.string
+
+import com.seanshubin.utility.string.TableFormat._
+
+class TableFormat(val contentSeparators: Content, val horizontalSeparators: Option[HorizontalSeparators]) {
+  def this(content: Content) = {
+    this(content, None)
+  }
+
+  def this(content: Content, top: Top, middle: Middle, bottom: Bottom) = {
+    this(content, Some(HorizontalSeparators(top, middle, bottom)))
+  }
+
+  private val emptyRow = Stream.continually("")
+
+  def createTable(originalRows: Seq[Seq[Any]]): Seq[String] = {
+    val paddedRows = makeAllRowsTheSameSize(originalRows, "")
+    val columns = paddedRows.transpose
+    val columnWidths = columns.map(maxWidthForColumn)
+    val formattedRows = formatRows(columnWidths, paddedRows, contentSeparators)
+    horizontalSeparators match {
+      case Some(HorizontalSeparators(topSeparators, middleSeparators, bottomSeparators)) =>
+        val top = makeRow(columnWidths, emptyRow, topSeparators)
+        val middle = makeRow(columnWidths, emptyRow, middleSeparators)
+        val bottom = makeRow(columnWidths, emptyRow, bottomSeparators)
+        Seq(top) ++ interleave(formattedRows, middle) ++ Seq(bottom)
+      case None =>
+        formattedRows
+    }
+  }
+}
+
+object TableFormat {
+  val BoxDrawingCharacters = new TableFormat(Content(" ", "║", "│", "║"), Top("═", "╔", "╤", "╗"), Middle("─", "╟", "┼", "╢"), Bottom("═", "╚", "╧", "╝"))
+  val AsciiDrawingCharacters = new TableFormat(Content(" ", "|", "|", "|"), Top("-", "/", "+", "\\"), Middle("-", "+", "+", "+"), Bottom("-", "\\", "+", "/"))
+  val CompactDrawingCharacters = new TableFormat(Content(" ", "", " ", ""))
+
+  trait VerticalSeparators {
+    def padding: String
+
+    def left: String
+
+    def center: String
+
+    def right: String
+  }
+
+  case class Content(padding: String, left: String, center: String, right: String) extends VerticalSeparators
+
+  case class Top(padding: String, left: String, center: String, right: String) extends VerticalSeparators
+
+  case class Middle(padding: String, left: String, center: String, right: String) extends VerticalSeparators
+
+  case class Bottom(padding: String, left: String, center: String, right: String) extends VerticalSeparators
+
+  case class HorizontalSeparators(top: Top, middle: Middle, bottom: Bottom)
+
+  sealed trait Justify
+
+  case class LeftJustify(x: Any) extends Justify
+
+  case class RightJustify(x: Any) extends Justify
+
+  private def makeAllRowsTheSameSize(rows: Seq[Seq[Any]], value: Any): Seq[Seq[Any]] = {
+    val rowSizes = rows.map(_.size)
+    val targetSize = if (rowSizes.isEmpty) 0 else rowSizes.max
+
+    def makeRowSameSize(row: Seq[Any]): Seq[Any] = {
+      val extraCells = makeExtraCells(targetSize - row.size, value)
+      row ++ extraCells
+    }
+
+    val sameSizeRows = rows.map(makeRowSameSize)
+    sameSizeRows
+  }
+
+  private def makeExtraCells(howMany: Int, contents: Any): Seq[Any] = {
+    (1 to howMany).map(_ => contents)
+  }
+
+  private val emptyRow = Stream.continually("")
+
+  private def makeRow(columnWidths: Seq[Int], data: Seq[Any], verticalSeparators: VerticalSeparators): String = {
+    val formattedCells = for {
+      (width, cell) <- columnWidths zip data
+    } yield {
+      formatCell(cell, width, verticalSeparators.padding)
+    }
+    formattedCells.mkString(verticalSeparators.left, verticalSeparators.center, verticalSeparators.right)
+  }
+
+  private def formatRows(columnWidths: Seq[Int], rows: Seq[Seq[Any]], verticalSeparators: VerticalSeparators): Seq[String] = {
+    val formatRow = makeRow(columnWidths, _: Seq[Any], verticalSeparators)
+    rows.map(formatRow)
+  }
+
+  private def formatCell(cell: Any, width: Int, padding: String): String = {
+    cell match {
+      case LeftJustify(x) => leftJustify(cellToString(x), width, padding)
+      case RightJustify(x) => rightJustify(cellToString(x), width, padding)
+      case null => rightJustify(cellToString(cell), width, padding)
+      case _: String => leftJustify(cellToString(cell), width, padding)
+      case _ => rightJustify(cellToString(cell), width, padding)
+    }
+  }
+
+  private def interleave[T](data: Seq[T], separator: T): Seq[T] = {
+    def combine(soFar: List[T], next: T): List[T] = {
+      next :: separator :: soFar
+    }
+
+    if (data.isEmpty) {
+      Seq()
+    } else {
+      data.tail.foldLeft(List(data.head))(combine).reverse
+    }
+  }
+
+  private def maxWidthForColumn(column: Seq[Any]): Int = {
+    column.map(cellWidth).max
+  }
+
+  private def cellWidth(cell: Any): Int = {
+    cellToString(cell).length
+  }
+
+  private def cellToString(cell: Any): String = {
+    cell match {
+      case null => "null"
+      case LeftJustify(x) => cellToString(x)
+      case RightJustify(x) => cellToString(x)
+      case x => x.toString
+    }
+  }
+
+  private def rightJustify(s: String, width: Int, padding: String = " "): String = {
+    paddingFor(s, width, padding) + s
+  }
+
+  private def leftJustify(s: String, width: Int, padding: String = " "): String = {
+    s + paddingFor(s, width, padding)
+  }
+
+  private def paddingFor(s: String, width: Int, padding: String): String = {
+    val quantity = width - s.length
+    padding * quantity
+  }
+}
--- string/src/test/scala/com/seanshubin/utility/string/TableFormatTest.scala
+++ string/src/test/scala/com/seanshubin/utility/string/TableFormatTest.scala
@@ -1,10 +1,12 @@
 package com.seanshubin.utility.string
 
-import com.seanshubin.utility.string.TableUtil.{LeftJustify, RightJustify}
+import com.seanshubin.utility.collection.SeqDifference
+import com.seanshubin.utility.string.TableFormat.{LeftJustify, RightJustify}
 import org.scalatest.FunSuite
 
-class TableUtilTest extends FunSuite {
-  test("table") {
+class TableFormatTest extends FunSuite {
+  test("box drawing characters") {
+    val tableFormat = TableFormat.BoxDrawingCharacters
     val input = Seq(
       Seq("Alice", "Bob", "Carol"),
       Seq("Dave", "Eve", "Mallory"),
@@ -18,12 +20,47 @@ class TableUtilTest extends FunSuite {
       "║Peggy│Trent│Wendy  ║",
       "╚═════╧═════╧═══════╝"
     )
-    val actual = TableUtil.createTable(input)
-    assert(actual === expected)
+    val actual = tableFormat.createTable(input)
+    assertLinesEqual(actual, expected)
+  }
+
+  test("plain text characters") {
+    val tableFormat = TableFormat.AsciiDrawingCharacters
+    val input = Seq(
+      Seq("Alice", "Bob", "Carol"),
+      Seq("Dave", "Eve", "Mallory"),
+      Seq("Peggy", "Trent", "Wendy"))
+    val expected = Seq(
+      "/-----+-----+-------\\",
+      "|Alice|Bob  |Carol  |",
+      "+-----+-----+-------+",
+      "|Dave |Eve  |Mallory|",
+      "+-----+-----+-------+",
+      "|Peggy|Trent|Wendy  |",
+      "\\-----+-----+-------/"
+    )
+    val actual = tableFormat.createTable(input)
+    assertLinesEqual(actual, expected)
+  }
+
+  test("compact") {
+    val tableFormat = TableFormat.CompactDrawingCharacters
+    val input = Seq(
+      Seq("Alice", "Bob", "Carol"),
+      Seq("Dave", "Eve", "Mallory"),
+      Seq("Peggy", "Trent", "Wendy"))
+    val expected = Seq(
+      "Alice Bob   Carol  ",
+      "Dave  Eve   Mallory",
+      "Peggy Trent Wendy  "
+    )
+    val actual = tableFormat.createTable(input)
+    assertLinesEqual(actual, expected)
   }
 
   test("left and right justify") {
-    import TableUtil.{LeftJustify, RightJustify}
+    import TableFormat.{LeftJustify, RightJustify}
+    val tableFormat = TableFormat.BoxDrawingCharacters
     val bigInt = BigInt(2)
     val bigDec = BigDecimal(3)
     val input = Seq(
@@ -48,25 +85,29 @@ class TableUtilTest extends FunSuite {
       "║3                       │                                3│                        3║",
       "╚════════════════════════╧═════════════════════════════════╧═════════════════════════╝"
     )
-    val actual = TableUtil.createTable(input)
-    assert(actual === expected)
+    val actual = tableFormat.createTable(input)
+    assertLinesEqual(actual, expected)
   }
 
   test("left and right justify something small") {
-    assert(TableUtil.createTable(Seq(Seq("a"))) === Seq("╔═╗", "║a║", "╚═╝"))
-    assert(TableUtil.createTable(Seq(Seq(LeftJustify("a")))) === Seq("╔═╗", "║a║", "╚═╝"))
-    assert(TableUtil.createTable(Seq(Seq(RightJustify("a")))) === Seq("╔═╗", "║a║", "╚═╝"))
+    val tableFormat = TableFormat.BoxDrawingCharacters
+    assert(tableFormat.createTable(Seq(Seq("a"))) === Seq("╔═╗", "║a║", "╚═╝"))
+    assert(tableFormat.createTable(Seq(Seq(LeftJustify("a")))) === Seq("╔═╗", "║a║", "╚═╝"))
+    assert(tableFormat.createTable(Seq(Seq(RightJustify("a")))) === Seq("╔═╗", "║a║", "╚═╝"))
   }
 
   test("no columns") {
-    assert(TableUtil.createTable(Seq(Seq())) === Seq("╔╗", "║║", "╚╝"))
+    val tableFormat = TableFormat.BoxDrawingCharacters
+    assert(tableFormat.createTable(Seq(Seq())) === Seq("╔╗", "║║", "╚╝"))
   }
 
   test("no rows") {
-    assert(TableUtil.createTable(Seq()) === Seq("╔╗", "╚╝"))
+    val tableFormat = TableFormat.BoxDrawingCharacters
+    assert(tableFormat.createTable(Seq()) === Seq("╔╗", "╚╝"))
   }
 
   test("replace empty cells with blank cells") {
+    val tableFormat = TableFormat.BoxDrawingCharacters
     val input = Seq(
       Seq("Alice", "Bob", "Carol"),
       Seq("Dave", "Eve"),
@@ -80,7 +121,12 @@ class TableUtilTest extends FunSuite {
       "║Peggy│Trent│Wendy║",
       "╚═════╧═════╧═════╝"
     )
-    val actual = TableUtil.createTable(input)
-    assert(actual === expected)
+    val actual = tableFormat.createTable(input)
+    assertLinesEqual(actual, expected)
+  }
+
+  def assertLinesEqual(actual: Seq[Any], expected: Seq[Any]): Unit = {
+    val seqDifference = SeqDifference.diff(actual, expected)
+    assert(seqDifference.isSame, seqDifference.messageLines.mkString("\n"))
   }
 }
